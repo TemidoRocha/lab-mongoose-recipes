2cec828260ff383f8c32142401cc6b831582c0c5
Manuel Rocha <manueltemidorocha@gmail.com>
2020-02-14 11:05:42 +0000

    first iteration

--- index.js
+++ index.js
@@ -12,8 +12,27 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => {
-    console.log(`Connected to the database: "${x.connections[0].name}"`);
+    console.log(`Connected to the database`);
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Lasagna',
+      level: 'Easy Peasy',
+      ingredients: ['pasta', 'meat', 'cheese', 'cream', 'tomato sauce'],
+      cuisine: 'Italian',
+      dishType: 'Dish',
+      duration: 45,
+      creator: 'Luca'
+    });
+  })
+  .then(recipeFile => {
+    return Recipe.find({ title: 'Lasagna' });
+  })
+  .then(whatComesFromPreviousThen => {
+    console.log(whatComesFromPreviousThen);
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Has disconnected');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
